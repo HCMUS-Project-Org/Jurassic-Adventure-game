51dbdd75191725aa168a38efe3cc6b26132528a1
QuannBluee <quannguyenthanh558@gmail.com>
2022-11-23 07:37:39 +0700

    feat(music): button SFX + BGM

--- Assets/Scripts/ClickyButton.cs
+++ Assets/Scripts/ClickyButton.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private Image _img;
     [SerializeField] private Sprite _default, _pressed;
-    [SerializeField] private AudioClip _compressClip, _uncompressClip;
-    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clickClip;
+    AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         _img.sprite = _pressed;
-        _audioSource.PlayOneShot(_compressClip);
+        _audioSource.PlayOneShot(_clickClip);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         _img.sprite = _default;
-        _audioSource.PlayOneShot(_uncompressClip);
     }
 }

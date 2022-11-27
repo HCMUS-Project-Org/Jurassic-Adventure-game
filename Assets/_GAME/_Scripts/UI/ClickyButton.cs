b9d512d7458a93674d9b7e3e6e329428bd0219a6
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-11-28 01:06:21 +0700

    update text

--- Assets/_GAME/_Scripts/UI/ClickyButton.cs
+++ Assets/_GAME/_Scripts/UI/ClickyButton.cs
@@ -1,30 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(AudioSource))]
-public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+[RequireComponent(typeof(AudioSource))] public class ClickyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    [SerializeField] private Image _img;
-    [SerializeField] private Sprite _default, _pressed;
-    [SerializeField] private AudioClip _clickClip;
-    AudioSource _audioSource;
+    [SerializeField] private Image       _img;
+    [SerializeField] private Sprite      _default, _pressed;
+    [SerializeField] private AudioClip   _clickClip;
+    private                  Transform   _buttonText;
+    private                  AudioSource _audioSource;
+
+    public bool hasText;
 
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+
+        if (hasText)
+            _buttonText = GetComponentInChildren<TextMeshProUGUI>().transform;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         _img.sprite = _pressed;
+
+        if (hasText)
+            _buttonText.position += 7 * Vector3.down;
+
         _audioSource.PlayOneShot(_clickClip);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         _img.sprite = _default;
+
+        if (hasText)
+            _buttonText.position += 7 * Vector3.up;
     }
 }

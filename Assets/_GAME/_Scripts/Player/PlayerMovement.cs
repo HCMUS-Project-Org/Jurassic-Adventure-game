f3410ae5961cc2e6f51d51ab48095c3ef3057e9f
QuannBluee <quannguyenthanh558@gmail.com>
2022-12-08 15:49:53 +0700

    feat: player basic movement

--- Assets/_GAME/_Scripts/Player/PlayerMovement.cs
+++ Assets/_GAME/_Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask platformLayerMask;
-    [SerializeField] private Sprite jumpSprite, moveSprite, dashSprite;
+    [SerializeField] private Animator animator;
+    // [SerializeField] private Sprite jumpSprite, moveSprite, dashSprite;
 
     public float _speed = 5;
     public float jumpVelocity = 1;
@@ -24,21 +25,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update() {
         if (IsGrounded()) {
+            
+
             // jump
             if (Input.GetButtonDown("Jump"))  {
                 _rigidbody2d.velocity = Vector2.up * jumpVelocity;
+                animator.SetBool("IsJump", true);
+                animator.SetBool("IsRun", false);
+
             }
             // dash
             if (Input.GetKey(KeyCode.LeftShift)) {
-                GetComponent<SpriteRenderer>().sprite = dashSprite;
+                // GetComponent<SpriteRenderer>().sprite = dashSprite;
+                animator.SetBool("IsCrouch", true);
             } 
             //move
             else {
-                GetComponent<SpriteRenderer>().sprite = moveSprite;
+                
+                // GetComponent<SpriteRenderer>().sprite = moveSprite;
             }
         } 
         else {
-            GetComponent<SpriteRenderer>().sprite = jumpSprite;
+            // GetComponent<SpriteRenderer>().sprite = jumpSprite;
+            // animator.SetBool("IsJump", false);
         }
 
         HandleMovement();
@@ -48,6 +57,18 @@ public class PlayerMovement : MonoBehaviour
     private void HandleMovement() {
         var movement = Input.GetAxis("Horizontal");
         
+        if (movement == 0f)
+            animator.SetBool("IsRun", false);
+        else if (movement != 0f && !IsGrounded()) {
+            animator.SetBool("IsRun", false);
+            animator.SetBool("IsJump", true);
+        }
+        else  {
+            animator.SetBool("IsRun", true);
+            animator.SetBool("IsJump", false);
+        }
+        
+
         moveRight = movement > 0 ? true : false;
 
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * _speed;
@@ -61,7 +82,7 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit2D = Physics2D.BoxCast(_boxCollider2d.bounds.center, _boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
-        
+        Debug.Log(raycastHit2D.collider);
         return raycastHit2D.collider != null;
     }
 

ada79efd2d79152995b18019248b082ac008b0d6
QuannBluee <quannguyenthanh558@gmail.com>
2022-12-09 13:56:58 +0700

    feat: follow player camera

---
+++ Assets/_GAME/_Scripts/UI/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    public Transform target;
+
+    // Cập nhật được gọi một lần trên mỗi khung hình
+    void Update ()
+    {
+        Vector3 newPos = new Vector3 (target.position.x, target.position.y + yOffset, transform.position.z);
+        transform.position = Vector3.Slerp (transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}

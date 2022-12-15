7c733833e51237b75106e3458a53923ddcd02311
QuannBluee <quannguyenthanh558@gmail.com>
2022-12-15 16:09:40 +0700

    feat (UI): Pause setting

--- Assets/_GAME/_Scripts/GamePlay/GameController.cs
+++ Assets/_GAME/_Scripts/GamePlay/GameController.cs
@@ -1,51 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField] private GameObject[] _enemies;
     [SerializeField] private GameObject _player;
-    [SerializeField] private GameObject _levelCompleteUI, _levelFailedUI, _pauseMenuUI, _shopUI;
-    
+    [SerializeField] private GameObject _levelCompleteUI, _levelFailedUI, _pauseMenuUI, _shopUI, _confirmExitUI;
+    [SerializeField] private GameObject _pauseBtn;
+    [SerializeField] private Sprite _pauseBtnImg, _resumeBtnImg;
     [SerializeField] private int _level;
     
     public static bool isGamePaused = false;
     public static bool isOpenShop = false;
+    public static bool isConfirmExit = false;
 
 
     void Update() {
-        // Pause game
+
+        // Exit game
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (isGamePaused) {
+            if (isGamePaused || isConfirmExit) 
                 ResumeGame();
-            }
-            else {
+            else 
+                ConfirmExitGame();
+        }
+
+        // Pause game
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (isOpenShop || isConfirmExit)
                 PauseGame();
-            }
+            else if (isGamePaused) 
+                ResumeGame();
+            else
+                PauseGame();
         }
 
         // Open Shop
-        if (Input.GetKeyDown(KeyCode.P)) {
-            if (isOpenShop) {
+        if (Input.GetKeyDown(KeyCode.O)) {
+            if (isOpenShop) 
                 ResumeGame();
-            }
-            else {
+            else 
                 OpenShop();
-            }
         }
     }
 
 
-    void OpenShop() {
-        Time.timeScale = 0f;
+    public void ConfirmExitGame() {
+        ResumeGame();
+        PauseGame();
+
+        if (_pauseMenuUI != null) 
+            _pauseMenuUI.SetActive(false);
+        
+        _confirmExitUI.SetActive(true);
+        
+        isConfirmExit = true;
+    }
+
+    public void OpenShop() {
+        ResumeGame();
+        PauseGame();
 
         if (_pauseMenuUI != null) 
             _pauseMenuUI.SetActive(false);
 
         _shopUI.SetActive(true);
-        
-        isGamePaused = true;
+
         isOpenShop = true;
     }
 
@@ -57,7 +79,11 @@ public class GameController : MonoBehaviour {
 
     public void PauseGame() {
         Debug.Log("Pause game");
-        
+        ResumeGame();
+
+        // change pause btn sprite
+        _pauseBtn.GetComponent<Image>().sprite = _resumeBtnImg;
+
         _pauseMenuUI.SetActive(true);
         
         Time.timeScale = 0f;
@@ -69,15 +95,18 @@ public class GameController : MonoBehaviour {
     public void ResumeGame() {
         Debug.Log("Resume game");
 
-        if (_pauseMenuUI != null) 
-            _pauseMenuUI.SetActive(false);
+        // change pause btn sprite
+        _pauseBtn.GetComponent<Image>().sprite = _pauseBtnImg;
 
+        _pauseMenuUI.SetActive(false);
         _shopUI.SetActive(false);
+        _confirmExitUI.SetActive(false);
         
         Time.timeScale = 1f;
         
         isGamePaused = false;
         isOpenShop = false;
+        isConfirmExit = false;
     }
 
 

82b00c0e1de0ee6a699e613ef422b6df81a68053
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-12-18 09:30:00 +0700

    added dash (Q)

--- Assets/_GAME/_Scripts/Player/PlayerMovement.cs
+++ Assets/_GAME/_Scripts/Player/PlayerMovement.cs
@@ -1,95 +1,134 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
-
-    [SerializeField] private LayerMask _platformLayerMask;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask        _platformLayerMask;
     [SerializeField] private PlayerController _playerController;
-    [SerializeField] private PlayerHealth _playerHealth;
-    private                  Rigidbody2D _rigidbody2d;
-    private                  BoxCollider2D _boxCollider2d;
-    private                  Vector2 _lookDirection = new Vector2(1, 0);
-    private                  Animator _animator;
-    
-    [SerializeField] private float _speed = 5;
+    [SerializeField] private PlayerHealth     _playerHealth;
+    private                  Rigidbody2D      _rigidbody2d;
+    private                  BoxCollider2D    _boxCollider2d;
+    private                  Vector2          _lookDirection = new Vector2(1, 0);
+    private                  Animator         _animator;
+
+    [SerializeField] private float _speed        = 5;
     [SerializeField] private float _jumpVelocity = 1;
-    private                  float _movement = 0f;
+    private                  float _movement     = 0f;
 
-    private bool _facingRight = true;
-    private bool _moveRight = true;
-    private bool _isCrouch = false;
+    private bool _facingRight  = true;
+    private bool _moveRight    = true;
+    private bool _isCrouch     = false;
     private bool _isCrouchDash = false;
-    
-    public GameObject projectilePrefab;
-    
 
-    private void Start() {
-        _rigidbody2d= transform.GetComponent<Rigidbody2D>();
+    public GameObject projectilePrefab;
+
+
+    private void Start()
+    {
+        _rigidbody2d   = transform.GetComponent<Rigidbody2D>();
         _boxCollider2d = transform.GetComponent<BoxCollider2D>();
-        _animator = GetComponent<PlayerController>().animator;
+        _animator      = GetComponent<PlayerController>().animator;
     }
 
 
     private void Update()
     {
         _isGrounded = IsGrounded;
-        
-        if (IsGrounded) {
+
+        if (IsGrounded)
+        {
             // jump
-            if (Input.GetButtonDown("Jump"))  {
+            if (Input.GetButtonDown("Jump"))
+            {
                 _rigidbody2d.velocity = Vector2.up * _jumpVelocity;
                 _animator.SetBool("IsJump", true);
                 _animator.SetBool("IsRun", false);
             }
-            
-            // crouch
-            _isCrouch = Input.GetKey(KeyCode.LeftControl);
 
-            // crouch dash
-            _isCrouchDash = Input.GetKey(KeyCode.LeftShift);
-        } 
+            if (!_dashing)
+            {
+                // crouch
+                _isCrouch = Input.GetKey(KeyCode.LeftControl);
 
+                // crouch dash
+                _isCrouchDash = Input.GetKey(KeyCode.LeftShift);
+            }
+        }
+        
         // melee
         if (Input.GetKeyDown(KeyCode.Mouse1))
-            _animator.SetTrigger("Melee");   
-        
+            _animator.SetTrigger("Melee");
+
         // skill
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
-            Launch();           
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Launch();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            if (!_dashing)
+                StartCoroutine(Dash());
 
         HandleMovement();
     }
 
+    private IEnumerator Dash()
+    {
+        _dashing = true;
+        var dashTime = .2f;
+        while ((dashTime -= Time.deltaTime) > 0)
+        {
+            _rigidbody2d.velocity = new Vector2((transform.localScale.x * forceToAdd * (dashTime / .2f)) * (_facingRight ? 1 : -1), 0);
+            yield return null;
+        }
 
-    private void HandleMovement() {
+        yield return new WaitForSeconds(dashCD);
+        _dashing = false;
+    }
+
+    private bool _dashing = false;
+
+    [SerializeField] private float forceToAdd;
+    private                  float dashCD = 1f;
+    private                  float currentDashCD;
+
+    private void HandleMovement()
+    {
         _movement = Input.GetAxis("Horizontal");
-        
-        if (!IsGrounded) {
+
+        if (!IsGrounded)
+        {
             _animator.SetBool("IsJump", true);
             _animator.SetBool("IsRun", false);
             _animator.SetBool("IsCrouch", false);
             _animator.SetBool("IsCrouchDash", false);
-        } 
-        else {
+        }
+        else
+        {
             _animator.SetBool("IsJump", false);
 
-            if (_movement == 0f) {
+            if (_movement == 0f)
+            {
                 _animator.SetBool("IsRun", false);
                 _animator.SetBool("IsCrouch", _isCrouch);
                 _animator.SetBool("IsCrouchDash", _isCrouchDash);
-
-            } else if (_movement != 0f) {
-                if (_isCrouch) {
+            }
+            else if (_movement != 0f)
+            {
+                if (_isCrouch)
+                {
                     _animator.SetBool("IsRun", false);
                     _animator.SetBool("IsCrouchDash", false);
                     _animator.SetBool("IsCrouch", true);
-
-                } else if (_isCrouchDash) {
+                }
+                else if (_isCrouchDash)
+                {
                     _animator.SetBool("IsRun", false);
                     _animator.SetBool("IsCrouch", false);
                     _animator.SetBool("IsCrouchDash", true);
-
-                } else {
+                }
+                else
+                {
                     _animator.SetBool("IsRun", true);
                     _animator.SetBool("IsCrouch", false);
                     _animator.SetBool("IsCrouchDash", false);
@@ -97,48 +136,50 @@ public class PlayerMovement : MonoBehaviour {
             }
         }
 
-        if (_movement == 0f) 
+        if (_movement == 0f)
             _moveRight = _facingRight;
         else
             _moveRight = _movement > 0 ? true : false;
 
         transform.position += new Vector3(_movement, 0, 0) * Time.deltaTime * _speed;
 
-        if ((_moveRight && !_facingRight) || (!_moveRight && _facingRight)) {
+        if ((_moveRight && !_facingRight) || (!_moveRight && _facingRight))
+        {
             Flip();
         }
     }
 
 
     private bool _isGrounded;
-    
+
     private bool IsGrounded =>
         Physics2D.BoxCast(_boxCollider2d.bounds.center, _boxCollider2d.bounds.size, 0f, Vector2.down, .1f, _platformLayerMask).collider != null;
-        
 
 
-
-    private void Flip() {
+    private void Flip()
+    {
         _facingRight = !_facingRight;
         transform.Rotate(0f, 180f, 0f);
     }
 
 
-    public void Launch() {
-        if (_playerController.health.currentMana <= 0) {
+    public void Launch()
+    {
+        if (_playerController.health.currentMana <= 0)
+        {
             _playerHealth.manaAnnounce.DisplayDialog();
             return;
         }
 
         _playerController.health.ChangeMana(-1);
 
-        _animator.SetTrigger("Skill");     
+        _animator.SetTrigger("Skill");
 
 
         _lookDirection.Set(_facingRight ? 1 : -1, 0);
 
         GameObject projectileObject = Instantiate(projectilePrefab, _rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        Projectile projectile       = projectileObject.GetComponent<Projectile>();
 
         projectile.Launch(_lookDirection, 300);
 

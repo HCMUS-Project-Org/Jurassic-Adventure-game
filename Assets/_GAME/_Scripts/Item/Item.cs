9b64b4ce4f1b4a69873d10e0bb06630230c6ef9d
QuannBluee <quannguyenthanh558@gmail.com>
2022-12-19 21:55:54 +0700

    Fix (UI): Shop's item Properties

--- Assets/_GAME/_Scripts/Item/Item.cs
+++ Assets/_GAME/_Scripts/Item/Item.cs
@@ -5,10 +5,6 @@ using UnityEngine.UI;
 using TMPro;
 
 public class Item : MonoBehaviour {
-    
-    [SerializeField] private GameObject _itemPropertyPanel;
-    [SerializeField] private TMPro.TextMeshProUGUI _tooltip, _price;
-
     public enum ItemType {
         Health,
         Mana,
@@ -17,9 +13,37 @@ public class Item : MonoBehaviour {
         Coin,
     }
 
+    [SerializeField] private GameObject _itemPropertyPanel;
+    private                  GameObject _itemIcon, _priceBoard, _coinIcon, _info;
+
+    private                  TMPro.TextMeshProUGUI _tooltipTxt, _priceTxt;
+
+    private                  Color _disableColor = new Color32(152, 152, 152, 255);
+    private                  Color _enableColor = Color.white;
+
     public ItemType itemType;
-    public int price;
     public int amount;
+
+
+    void Start() {
+        _itemIcon = transform.GetChild(0).gameObject;   
+        _priceBoard = transform.GetChild(1).gameObject;
+        _info = transform.GetChild(2).gameObject;
+
+        _coinIcon = _priceBoard.transform.GetChild(1).gameObject;
+        _priceTxt = _priceBoard.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+        _tooltipTxt = _info.transform.GetChild(0).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+        
+        _itemIcon.GetComponent<Image>().sprite = this.GetSprite(this.itemType);
+        _tooltipTxt.text = this.itemType.ToString();
+        _priceTxt.text = this.GetPrice(this.itemType).ToString();
+    }
+
+
+    void Update() {
+        DisableItem(IsCanBuy());
+    }
+
 
     public Sprite GetSprite(ItemType itemType) {
         switch (itemType) {
@@ -38,6 +62,7 @@ public class Item : MonoBehaviour {
         }
     }
 
+
     public int GetPrice(ItemType itemType) {
         switch (itemType) {
             case ItemType.Health:
@@ -55,6 +80,7 @@ public class Item : MonoBehaviour {
         }
     }
 
+
     public string GetDescription(ItemType itemType) {
         switch (itemType) {
             case ItemType.Health:
@@ -71,10 +97,38 @@ public class Item : MonoBehaviour {
                 return null;
         }
     }
+    
+
+    void DisableItem(bool isCanBuy) {
+        if (isCanBuy) {
+            gameObject.GetComponent<ClickyButton>().enabled = true;
+            gameObject.GetComponent<CustomEvent>().enabled = true;
+            gameObject.GetComponent<Button>().enabled = true;
+
+            gameObject.GetComponent<Image>().color = _enableColor;
+            _itemIcon.GetComponent<Image>().color = _enableColor;
+            _priceBoard.GetComponent<Image>().color = _enableColor;
+            _coinIcon.GetComponent<Image>().color = _enableColor;
+            _priceTxt.color = _enableColor;
+        } 
+        else {
+            gameObject.GetComponent<ClickyButton>().enabled = false;
+            gameObject.GetComponent<CustomEvent>().enabled = false;
+            gameObject.GetComponent<Button>().enabled = false;
+
+            gameObject.GetComponent<Image>().color = _disableColor;
+            _itemIcon.GetComponent<Image>().color = _disableColor;
+            _priceBoard.GetComponent<Image>().color = _disableColor;
+            _coinIcon.GetComponent<Image>().color = _disableColor;
+            _priceTxt.color = _disableColor;
+        }
+    }
+
 
     public void SelectItem() {
         ChangePropertyPanelDetail();
     }
+
 
     void ChangePropertyPanelDetail() {
         TMPro.TextMeshProUGUI itemName, description;
@@ -91,13 +145,13 @@ public class Item : MonoBehaviour {
 
 
     bool IsCanBuy() {
-        return PlayerController.budget >= this.price;
+        return PlayerController.budget >=  this.GetPrice(this.itemType);
     }
 
 
     public void Buy() {
         if (IsCanBuy()) {
-            PlayerController.budget -= this.price;
+            PlayerController.budget -= this.GetPrice(this.itemType);
             InventoryManager.instance.AddItem(this);
         } else {
             Debug.Log("Not enough money");

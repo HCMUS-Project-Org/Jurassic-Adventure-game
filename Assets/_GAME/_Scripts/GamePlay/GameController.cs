d040c0f719e0abee5b6c4961857c87842c28c1cc
QuannBluee <quannguyenthanh558@gmail.com>
2023-01-01 15:32:34 +0700

    Fix: merge conflict

--- Assets/_GAME/_Scripts/GamePlay/GameController.cs
+++ Assets/_GAME/_Scripts/GamePlay/GameController.cs
@@ -2,36 +2,36 @@ using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
+
     [SerializeField] private GameObject[]     _enemies;
     [SerializeField] private GameObject       _player;
     [SerializeField] private GameObject       _levelCompleteUI, _levelFailedUI;
-    [SerializeField] private GameObject       _pauseMenuUI,     _shopUI, _inventoryUI, _confirmExitUI;
+    [SerializeField] private GameObject       _pauseMenuUI, _shopUI, _inventoryUI, _mapUI, _confirmExitUI;
     [SerializeField] private GameObject       _pauseBtn;
+    [SerializeField] private GameObject       _zoomCameraController;
     [SerializeField] private Sprite           _pauseBtnImg, _resumeBtnImg;
     [SerializeField] private UIInventory      _inventoryMenu;
     private                  InventoryManager _inventoryManager;
     private                  EquipmentManager _equipmentManager;
+
 
     [SerializeField] private int _level;
 
     private bool _isGamePaused    = false;
     private bool _isOpenShop      = false;
     private bool _isOpenInventory = false;
+    private bool _isOpenMap = false;
     private bool _isConfirmExit   = false;
 
-    void Start()
-    {
+    void Start() {
         _inventoryManager = FindObjectOfType(typeof(InventoryManager)) as InventoryManager;
         _equipmentManager = FindObjectOfType(typeof(EquipmentManager)) as EquipmentManager;
     }
 
-    void Update()
-    {
+    void Update() {
         // Exit game
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             if (_isGamePaused || _isConfirmExit)
                 ResumeGame();
             else
@@ -41,7 +41,7 @@ public class GameController : MonoBehaviour
         // Pause game
         if (Input.GetKeyDown(KeyCode.P))
         {
-            if (_isOpenShop || _isConfirmExit || _isOpenInventory)
+            if (_isOpenShop || _isConfirmExit || _isOpenInventory || _isOpenMap)
                 PauseGame();
             else if (_isGamePaused)
                 ResumeGame();
@@ -65,6 +65,15 @@ public class GameController : MonoBehaviour
                 ResumeGame();
             else
                 OpenInventory();
+        }
+
+        // Open Map
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (_isOpenMap)
+                ResumeGame();
+            else
+                OpenMap();
         }
     }
 
@@ -90,6 +99,18 @@ public class GameController : MonoBehaviour
         _shopUI.SetActive(true);
 
         _isOpenShop = true;
+    }
+
+    public void OpenMap()
+    {
+        ResumeGame();
+        PauseGame();
+
+        _pauseMenuUI.SetActive(false);
+        _mapUI.SetActive(true);
+        _zoomCameraController.SetActive(true);
+
+        _isOpenMap = true;
     }
 
 
@@ -146,7 +167,9 @@ public class GameController : MonoBehaviour
         _pauseMenuUI.SetActive(false);
         _shopUI.SetActive(false);
         _inventoryUI.SetActive(false);
+        _mapUI.SetActive(false);
         _confirmExitUI.SetActive(false);
+        _zoomCameraController.SetActive(false);
 
         _player.SetActive(true);
 
@@ -155,6 +178,7 @@ public class GameController : MonoBehaviour
         _isGamePaused    = false;
         _isOpenShop      = false;
         _isOpenInventory = false;
+        _isOpenMap = false;
         _isConfirmExit   = false;
     }
 
